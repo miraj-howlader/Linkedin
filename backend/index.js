41966a53b4b4726077366efa368c032c7caad72a
Miraj Howlader <110389768+miraj-howlader@users.noreply.github.com>
2026-02-04 14:48:22 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express()
 const server = http.createServer(app)
 export const io = new Server(server,{
     cors:({
-        origin:'http://localhost:5173',
+        origin:'https://linkedin-frontend-8rer.onrender.com',
         credentials:true
     })
 })
@@ -32,7 +32,7 @@ app.get('/',(req,res)=>{
     res.send('Server is Live')
 })
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://linkedin-frontend-8rer.onrender.com',
     credentials:true
 }))
 
